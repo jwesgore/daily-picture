b10c4977e0cc142598f5256380e937413c716346
Gore, James <jwesgore@gmail.com>
2026-01-06 21:31:34 -0600

    make mobile navbar better

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -7,6 +7,8 @@ const NavBar: React.FC = () => {
   const [open, setOpen] = useState(false);
   const { user } = useAuth();
 
+  const closeMenu = () => setOpen(false);
+
   return (
     <nav className="navbar">
       <div className="navbar__brand">
@@ -25,17 +27,20 @@ const NavBar: React.FC = () => {
       </button>
 
       <ul className={`navbar__links ${open ? 'open' : ''}`}>
-        <li><a href="/teams">Meet the Teams</a></li>
-        <li><a href="/scoreboard">Scoreboard</a></li>
-        <li><a href="/about">About</a></li>
-        <li>
-          <Link to="/profile" className="navbar__profile-link">
+        <li><a href="/teams" onClick={closeMenu}>Meet the Teams</a></li>
+        <li><a href="/scoreboard" onClick={closeMenu}>Scoreboard</a></li>
+        <li><a href="/about" onClick={closeMenu}>About</a></li>
+        <li className="navbar__profile-item">
+          <Link to="/profile" className="navbar__profile-link" onClick={closeMenu}>
             {user ? (
-              <span className="navbar__avatar" aria-label="Profile">
-                {user.avatar_url
-                  ? <img src={user.avatar_url} alt="Profile" />
-                  : (user.username?.[0]?.toUpperCase() ?? 'P')}
-              </span>
+              <>
+                <span className="navbar__avatar" aria-label="Profile">
+                  {user.avatar_url
+                    ? <img src={user.avatar_url} alt="Profile" />
+                    : (user.username?.[0]?.toUpperCase() ?? 'P')}
+                </span>
+                <span className="navbar__username">{user.username}</span>
+              </>
             ) : (
               'Sign In'
             )}
